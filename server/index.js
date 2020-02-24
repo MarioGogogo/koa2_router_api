f90f3b2318fe0f625522f480c0badf166f3999f9
lovewcc <148373644@qq.com>
2020-02-24 09:40:16 +0800

    实现一个比较简单的router

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
 const Koa = require('koa');
 
-//创建实例
-const app = new Koa();
-
 // 路由路径
-const index = require('./routes');
+const index = require('./routes/index.js');
 
-//router
-app.use(index.routes(), index.allowedMethods());
+(async () => {
+  //创建实例
+  const app = new Koa();
+  await useMiddlewares(app);
+  //router
+  app.use(index.routes(), index.allowedMethods());
+  // error logger
+  app.on('error', (err, ctx) => {
+    console.log('error occured:', err);
+  });
 
-// error logger
-app.on('error', (err, ctx) => {
-  console.log('error occured:', err);
-});
-
-app.listen(4455, () => {
-  console.log('http://127.0.0.1:4455 is runing');
-});
+  app.listen(4455, () => {
+    console.log('http://127.0.0.1:4455 is runing');
+  });
+})();
